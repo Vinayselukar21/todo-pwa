b8986ce0ef2efe8dd1fed82740f5da4d140c6681
Vinay Selukar <vselukar2102@gmail.com>
2023-06-29 16:18:47 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,35 @@
-const withOffline = require('next-offline')
+const withPWA = require("next-pwa")({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
 
-module.exports = withOffline({
-	workboxOpts: {
-		swDest: 'static/service-worker.js',
-		runtimeCaching: [
-			{
-				urlPattern: /[.](png|jpg|ico|css)/,
-				handler: 'CacheFirst',
-				options: {
-					cacheName: 'assets-cache',
-					cacheableResponse: {
-						statuses: [0, 200],
-					},
-				},
-			},
-			{
-				urlPattern: /^http.*/,
-				handler: 'NetworkFirst',
-				options: {
-					cacheName: 'http-cache',
-				},
-			},
-		],
-	},
-})
+const withOffline = require("next-offline")({
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /[.](png|jpg|ico|css)/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "assets-cache",
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+      {
+        urlPattern: /^http.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "http-cache",
+        },
+      },
+    ],
+  },
+});
+
+module.exports = { withPWA, withOffline };
